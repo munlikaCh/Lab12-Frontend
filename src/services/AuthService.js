caf3b70310ac38a431b55e158f8c018b4ae19243
munlikaCh <munlika_ch@cmu.ac.th>
2021-10-10 23:36:39 +0700

    4.11

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,4 +1,5 @@
 import apiClient from '@/services/AxiosClient.js'
+import GStore from '@/store'
 
 export default {
     login(user) {
@@ -6,11 +7,22 @@ export default {
             username: user.username,
             password: user.password
         }).then((response) => {
-            localStorage.setItem('token',response.data.token)
-            return Promise.resolve(response.data)           
+            localStorage.setItem('token', response.data.token)
+            localStorage.setItem('user',JSON.stringify(response.data.user))
+            GStore.currentUser = response.data.user 
+            return Promise.resolve(response.data)          
         }).catch((error) => {
             return Promise.reject(error)
         })
-       
+    },
+    logout() {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        GStore.currentUser = null
+    },
+    getUser(){
+        return JSON.parse(localStorage.getItem('user'))
     }
+    
 }
+
